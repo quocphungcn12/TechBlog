85b50e580e9feca67cf2760f7b38bd23f2b44442
PHUNG-199676\PHUNG-199676 <quocphungcn12@gmail.com>
2024-04-15 12:52:52 +0700

    commit

--- tech-blog-ui/src/app/shared/constants/url.constants.ts
+++ tech-blog-ui/src/app/shared/constants/url.constants.ts
@@ -1,5 +1,5 @@
 export class UrlConstants {
-    public static readonly LOGIN = "/auth/login";
-    public static readonly HOME = "/dashboard";
-    public static readonly ACCESS_DENIED = "/auth/403";
+  public static readonly LOGIN = '/auth/login';
+  public static readonly HOME = '/dashboard';
+  public static readonly ACCESS_DENIED = '/auth/403';
 }
